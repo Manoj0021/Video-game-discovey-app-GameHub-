d2455b330717b04815a21bed2bd1ebddc66a7d36
Manoj <manojhn036@gmail.com>
2023-07-13 08:04:19 +0530

    api key updated

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "f98f1d35c07640e7b2b5033f9c3df5b8",
   },
 });
